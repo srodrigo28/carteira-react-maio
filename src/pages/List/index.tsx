9edd92f428d5866e36520f75597eedb6f7e91b2b
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-02 15:51:58 -0300

    create-filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,145 +1,40 @@
 import ContentHeader from "../../components/ContentHeader";
 import HistoryFianceCard from "../../components/HistoryFinanceCard";
 import SelectInput from "../../components/SelectInput";
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 
 const List: React.FC = () => {
-    const options = [
-        { value: '2023', label: '2023' },
-        { value: '2022', label: '2022' },
-        { value: '2021', label: '2021' },
-        { value: '2020', label: '2020' }
-    ]  
+    const months = [
+        { value: 7, label: 'Julho' },
+        { value: 8, label: 'Agosto' },
+        { value: 9, label: 'Setembro' },
+        { value: 10, label: 'Outubro' }
+    ]
+    const years = [
+        { value: 2018, label: 2018 },
+        { value: 2019, label: 2019 },
+        { value: 2020, label: 2020 },
+        { value: 2021, label: 2021 }
+    ]
     return (
         <Container>
-                <ContentHeader title='Entradas' lineColor='pink'>
-                    <SelectInput options={options} />
+            <ContentHeader title='Entradas' lineColor='#E44C4E'>    
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
-
+            <Filters>
+                <button className="tag-filters tag-filter-recurrent" type="button">Recorrentes</button>
+                <button className="tag-filters tag-filter-eventual" type="button">Eventuais</button>
+            </Filters>
             <Content>
                 <HistoryFianceCard
-                     
-                    tagColor="#E44C4E"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="green"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="#E44C4E"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="green"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="#E44C4E"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="green"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="#E44C4E"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="green"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="#E44C4E"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="green"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="#E44C4E"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="green"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="#E44C4E"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
-                    tagColor="green"
-                    title="Aluguel"
-                    subtitle="01/05/2023"
-                    amount="R$ 1.000,00"
-                />
-
-                <HistoryFianceCard
-                     
                     tagColor="#E44C4E"
                     title="Aluguel"
                     subtitle="01/05/2023"
                     amount="R$ 1.000,00"
                 />
             </Content>
-            
         </Container>
     )
 }
-
 export default List;
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -3,3 +3,47 @@ import styled from "styled-components";
 export const Container = styled.div``;
 
 export const Content = styled.div``;
+
+export const Filters = styled.div`
+    width: 100%;
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+
+    .tag-filters{
+        font-size: 18px;
+        font-weight: 500;
+        background-color: transparent;
+        color: ${ props => props.theme.colors.white };
+
+        transition: opacity .1s;
+
+        :hover{
+            opacity: .5;
+        }
+
+        ::after{ 
+            content: '';
+            display: block;
+            width: 55px;
+            margin-left: 1px;
+            border-bottom: 10px solid ${ props => props.theme.colors.warning };
+        }
+    }
+    .tag-filter-recurrent::after{
+        content: '';
+            display: block;
+            width: 55px;
+            margin-left: 1px;
+            border-bottom: 10px solid ${ props => props.theme.colors.warning };
+    }
+    .tag-filter-eventual::after{
+        content: '';
+            display: block;
+            width: 55px;
+            margin-left: 1px;
+            border-bottom: 10px solid ${ props => props.theme.colors.success };
+    }
+`;
